e1edf1f362ef2bffe563d03d3ae685aa04b26556
camilazci <camilazucchi@outlook.com>
2024-03-14 17:36:33 -0300

    feat: add attribute department to seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -11,15 +11,17 @@ public class Seller implements Serializable {
     private String email;
     private Date birthDate;
     private Double baseSalary;
+    private Department department;
 
     public Seller() {}
 
-    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
 
     public Integer getId() {
@@ -60,6 +62,14 @@ public class Seller implements Serializable {
 
     public void setBaseSalary(Double baseSalary) {
         this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 
     @Override
