5f28cce6decd7f339ffb35f39b075dc2b3217f15
camilazci <camilazucchi@outlook.com>
2024-03-14 17:33:11 -0300

    feat: implement seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,7 +1,88 @@
 package model.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+public class Seller implements Serializable {
 
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    public Seller() {}
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Seller seller = (Seller) object;
+        return Objects.equals(id, seller.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Seller - " +
+                "Id: " + id +
+                " | Name: " + name +
+                " | E-mail: " + email +
+                " | Birthdate: " + birthDate +
+                " | Base salary: " + baseSalary;
+    }
 
 }
