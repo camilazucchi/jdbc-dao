c4b3d89c66cf2f11b8820532eb4d25a4b4397a31
camilazci <camilazucchi@outlook.com>
2024-03-15 15:37:12 -0300

    feat: add simple test for seller findById method

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,11 +6,14 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("---- TEST #1: seller findById method ----");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
