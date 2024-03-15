fc413b4074493fa9d8538c5a32dad9ff82f01db7
camilazci <camilazucchi@outlook.com>
2024-03-15 15:02:53 -0300

    refactor: add private constructor for dao factory utility class

--- src/model/DaoFactory.java
+++ src/model/DaoFactory.java
@@ -8,6 +8,17 @@ import model.dao.impl.SellerDaoJDBC;
  * a lógica de criação dessas instâncias diretamente no código cliente. O objetivo principal da "DaoFactory" é
  * encapsular a criação de objetos DAO, promovendo a flexibilidade, manutenibilidade e teste da aplicação. */
 public class DaoFactory {
+    /* SonarLint: Add a private constructor to hide the implicit public one.
+    * O que isso significa?
+    * Quando uma classe não declara explicitamente um construtor, o Java fornece um construtor público padrão
+    * implícito. Isso significa que qualquer código externo pode criar instâncias dessa classe usando esse construtor
+    * padrão.
+    * Se desejamos controlar como as instâncias da classe são criadas e limitar o acesso a elas, é uma boa prática
+    * declarar um construtor privado e fornecer métodos estáticos ou de fábrica para criar instâncias. Isso é conhecido
+    * como padrão de projeto Singleton. */
+    private DaoFactory() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated ");
+    }
 
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
