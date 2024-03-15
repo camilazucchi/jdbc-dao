60b4554457a0f7bab2132a3c7d4b8461d594bafa
camilazci <camilazucchi@outlook.com>
2024-03-15 16:35:43 -0300

    feat: add simple test for find by department method

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,10 @@ import model.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Main {
@@ -16,6 +18,12 @@ public class Main {
         System.out.println("---- TEST #1: seller findById method ----");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println();
+        System.out.println("---- TEST #2: seller findByDepartment method ----");
+        Department dep = new Department(1, null);
+        List<Seller> sellers = sellerDao.findByDepartment(dep);
+        sellers.forEach(System.out::println);
 
     }
 }
