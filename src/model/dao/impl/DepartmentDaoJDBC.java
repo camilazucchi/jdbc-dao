6ee8fd46af293950b27d8e889cae625e5ac13ff0
camilazci <camilazucchi@outlook.com>
2024-03-19 11:28:16 -0300

    feat: implement findById method

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,92 @@
+package model.dao.impl;
+
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private final Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        try (PreparedStatement st = conn.prepareStatement("INSERT INTO department " +
+                "(name) " +
+                "VALUES (?) ",
+                Statement.RETURN_GENERATED_KEYS)) {
+            st.setString(1, obj.getName());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void update(Department obj) {
+        try (PreparedStatement st = conn.prepareStatement("UPDATE department " +
+                "SET name = ? " +
+                "WHERE id = ?")) {
+            st.setString(1, obj.getName());
+            st.setInt(2, obj.getId());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        try (PreparedStatement st = conn.prepareStatement("SELECT * " +
+                "FROM department " +
+                "WHERE id = ?")) {
+            st.setInt(1, id);
+            return executeQueryAndProcessResult(st);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    private Department executeQueryAndProcessResult(PreparedStatement st) {
+        try (ResultSet rs = st.executeQuery()) {
+            if (rs.next()) {
+                return instantiateDepartment(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("id"));
+        dep.setName(rs.getString("name"));
+        return dep;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
