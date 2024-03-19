826a2adbf1355094f73b0e520f95dcb235069696
camilazci <camilazucchi@outlook.com>
2024-03-19 10:45:55 -0300

    feat: implement update method

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -65,8 +65,23 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
 
+    // Método simples que realiza a atualização de um vendedor no banco de dados:
     @Override
     public void update(Seller obj) {
+        try (PreparedStatement st = conn.prepareStatement("UPDATE seller " +
+                "SET name = ?, email = ?, bithdate = ?, basesalary = ?, departmentid = ? " +
+                "WHERE id = ?")) {
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
